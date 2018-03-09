c016eb7e0313b6822264abb3ca5326e2c4582ae5
John Wu <john.wu@mail.mcgill.ca>
2018-03-09 14:55:00 -0500

    Comminting partially working changes

--- Lab-2/python/lab2.py
+++ Lab-2/python/lab2.py
@@ -1,18 +1,4 @@
-import re
-import numpy as np
-import struct
-from struct import *
-import struct
-
-X_INPUT = 'lab2-x.txt'
-Y_INPUT = 'lab2-y.txt'
-
-x_string = open(X_INPUT, 'r', encoding="utf-8").readlines()[0].split(' ')
-y_string = open(Y_INPUT, 'r', encoding="utf-8").readlines()[0].split(' ')
-
-x_values = [] # [float, sign, integer, fraction]
-y_values = [] # [float, sign, integer, fraction]
-
+''' Converts the fractional component to a binary string '''
 def convertFractionToBinary(digits):
     fraction = float('0.' + digits)
     binary = ''
@@ -25,60 +11,60 @@ def convertFractionToBinary(digits):
             fraction = fraction - 1
     return binary
 
-# def twos_comp_w(val, precision):
-#     if(val == ''):
-#         val = 0
-#     else:
-#         val = int(val, 2)
-#     val = ~val  # flip the bits
-#     val += 1  # add one (two's complement method)
-#     p = '0' + str(precision - 1) + 'b'
-#     binary = str(format(val, p))
-#     binary = binary.replace('-', '1')
-#     return binary
-
-def twos_comp(val, precision):
-    p = '0' + str(precision - 1) + 'b'
-    p = '0' + str(precision - 2) + 'b'
-    if (val == ''):
-        val = 0
-    else:
-        val = int(val, 2)
-    if (val == 0):
-        print(str(format(0,p)))
-        return str(format(0,p))
-    val = val - (1 << precision - 2)
-    p = '0' + str(precision - 1) + 'b'
-    binary = str(format(val, p))
-    binary = binary.replace('-', '')
+''' Converts the w component to its twos complement with the correct precision '''
+def twos_comp_w(val, precision):
+    p = '0' + str(precision) + 'b'
+    converted = bin(int(val) % (1 << precision))
+    binary = format(int(converted, 2), p)
     return binary
 
+''' Converts the f component to its twos complement with the correct precision '''
+def twos_comp_f(val, precision):
+    while (len(val) <= precision):
+        val = val + '0'
+    return val
+
+# Input files
+X_INPUT = 'lab2-x.txt'
+Y_INPUT = 'lab2-y.txt'
+
+# Read input files
+x_string = open(X_INPUT, 'r', encoding="utf-8").readlines()[0].split(' ')
+y_string = open(Y_INPUT, 'r', encoding="utf-8").readlines()[0].split(' ')
+
+# Store x andy values in a vector format
+x_values = [] # [float, sign, integer, fraction]
+y_values = [] # [float, sign, integer, fraction]
+
+# Read x values and store in the vector format
 for i in range(0, len(x_string)-1):
     value = x_string[i]
-    vector = ['1', '0', '0', '0']
+    vector = ['1', '', '0', '0']
     vector[0] = value
     if (value[0] == '-'):
         value = value.replace('-', '')
-        vector[1] = '1'
+        vector[1] = '-'
     split = value.split('.')
     vector[2] = split[0]
     if (len(split) == 2):
         vector[3] = split[1]
     x_values.append(vector)
 
+# Read y values and store in the vector format
 for i in range(0, len(y_string)-1):
     value = y_string[i]
-    vector = ['1', '0', '0', '0']
+    vector = ['1', '', '0', '0']
     vector[0] = value
     if (value[0] == '-'):
         value = value.replace('-', '')
-        vector[1] = '1'
+        vector[1] = '-'
     split = value.split('.')
     vector[2] = split[0]
     if (len(split) == 2):
         vector[3] = split[1]
     y_values.append(vector)
 
+# Calculate the precision for w and f values
 w_precision = 1
 f_precision = 0
 for i in range(len(x_values)):
@@ -90,7 +76,6 @@ for i in range(len(x_values)):
         w_precision = w
     if (f > f_precision):
         f_precision = f
-
 for i in range(len(y_values)):
     w = 1
     f = 0
@@ -100,27 +85,26 @@ for i in range(len(y_values)):
         w_precision = w
     if (f > f_precision):
         f_precision = f
-
-
-
 print('W Precision: ', w_precision)
 print('F Precision: ', f_precision)
 
+# Store the twos complements output
 x_output = ''
 y_output = ''
 
+# Convert all x values to twos complements
 for i in range(len(x_values)):
-    complement = x_values[i][1]
-    complement += twos_comp((str(bin(int((x_values[i][2]))))).replace('0b',''), w_precision+1)
-    complement += twos_comp(convertFractionToBinary(x_values[i][3]), f_precision+2)
-    x_output += complement + '\n'
+    complement = twos_comp_w(x_values[i][1] + x_values[i][2], w_precision)
+    complement = complement + twos_comp_f(convertFractionToBinary(x_values[i][3]), f_precision)
+    x_output = x_output + complement + '\n'
 
+# Converts all y values to twos complements
 for i in range(len(y_values)):
-    complement = y_values[i][1]
-    complement += twos_comp((str(bin(int((y_values[i][2]))))).replace('0b',''), w_precision+1)
-    complement += twos_comp(convertFractionToBinary(y_values[i][3]), f_precision+2)
-    y_output += complement + '\n'
+    complement = twos_comp_w(y_values[i][1] + y_values[i][2], w_precision)
+    complement = complement + twos_comp_f(convertFractionToBinary(y_values[i][3]), f_precision)
+    y_output = y_output + complement + '\n'
 
+# Write the result to an output file
 writer = open('lab2-x-fixed-point.txt', 'w')
 writer.write(x_output)
 writer = open('lab2-y-fixed-point.txt', 'w')
