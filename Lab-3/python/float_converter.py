d6455924a6dfb971e23f1f896d0fb0fbcf3db64c
John Wu <john.wu@mail.mcgill.ca>
2018-03-23 12:57:19 -0400

    Updated python file

--- Lab-3/python/float_converter.py
+++ Lab-3/python/float_converter.py
@@ -11,35 +11,6 @@ def convertFractionToBinary(digits):
             fraction = fraction - 1
     return binary
 
-''' Converts the w component to its twos complement with the correct precision '''
-def twos_comp_w(val, precision):
-    p = '0' + str(precision) + 'b'
-    converted = bin(int(val) % (1 << precision))
-    binary = format(int(converted, 2), p)
-    return binary
-
-''' Converts the f component to its twos complement with the correct precision '''
-def twos_comp_f(val, precision):
-    while (len(val) < precision):
-        val = val + '0'
-    return val[:precision]
-
-''' Calculate the precision based on the start and end indices '''
-def calculatePrecision(input_values, start, end):
-    # Calculate the precision for w and f values
-    w_precision = 1
-    f_precision = 0
-    for i in range(start, end):
-        w = 1
-        f = 0
-        w += len(bin(int(input_values[i][2]))) - 2  # precision of decimal
-        f += len(convertFractionToBinary(input_values[i][3]))  # precision of fraction
-        if (w > w_precision):
-            w_precision = w
-        if (f > f_precision):
-            f_precision = f
-    return w_precision, f_precision
-
 ''' Calculates twos complements based on precision and indices to include '''
 def calculateTwosComplements(input_values, w_precision, f_precision, start, end):
     # Store the twos complements output
@@ -103,21 +74,11 @@ for i in range(0, len(c_string) - 1):
         vector[3] = split[1]
     c_values.append(vector)
 
-# Calculate precision for main batch
-w, f = calculatePrecision(x_values, 0, len(x_values))
-print('Input W Precision: ', w)
-print('Input F Precision: ', f, '\n')
-
-# Calculate twos complements based on precision for main batch and writes to file
+# Calculate twos complements and writes to file
 output = calculateTwosComplements(x_values, 1, 15, 0, len(x_values))
 writeOutput(output, 'lab3-in-fixed-point.txt')
 
-# Calculate precision for main batch
-w, f = calculatePrecision(c_values, 0, len(c_values))
-print('Coefficient W Precision: ', w)
-print('Coefficient F Precision: ', f, '\n')
-
-# Calculate twos complements based on precision for main batch and writes to file
+# Calculate twos complements and writes to file
 output = calculateTwosComplements(c_values, 1, 15, 0, len(c_values))
 writeOutput(output, 'lab3-coeff-fixed-point.txt')
 
